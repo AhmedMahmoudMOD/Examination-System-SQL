7cda6df87600db47c60b19d5f507cefe72e425f2
abdellatief <abdellatiefhamed00@gmail.com>
2024-03-19 09:30:42 +0200

    ready for push

--- Examination System Web App/Controllers/StudentController.cs
+++ Examination System Web App/Controllers/StudentController.cs
@@ -20,7 +20,7 @@ namespace Examination_System_Web_App.Controllers
             ViewBag.student =  stdrepo.GetStudent(id.Value);
             return View(model);
         }
-
+        //thing
         public IActionResult exams(int? id)
         {
             if(id==null)
